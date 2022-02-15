300ea1958a71e003a660abf57643b330cc0a9fcb
stfDeveloper <sstefancosmin@icloud.com>
2022-02-15 12:57:46 +0100

    first commit

---
+++ intro.php
@@ -0,0 +1,12 @@
+<?php
+
+$squadre = [
+    ["name" =>"Olimpia","points"=> 53],
+    ["name" =>"Virtus" ,"points"=> 34],
+    ["name" =>"Lakers" ,"points"=> 56],
+    ["name" =>"Brindisi","points"=> 23],
+    ["name" =>"Allianz","points"=> 43],
+    ["name" =>"Fortitudo","points"=> 65],
+]
+?>
+<h1> <?php echo $squadre ?></h1>
