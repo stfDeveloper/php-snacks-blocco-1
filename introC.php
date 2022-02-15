449994cd709083da7b7003b6ff2b3b8294602525
stfDeveloper <sstefancosmin@icloud.com>
2022-02-15 16:15:52 +0100

    snack C

--- introC.php
+++ introC.php
@@ -1,5 +1,17 @@
 <!-- Creare un array con 15 numeri casuali, 
 tenendo conto che l’array non dovrà contenere lo stesso numero più di una volta -->
 <?php 
-
+$numbers = []
 ?>
+
+<p>
+<?php 
+    for ($i=0; $i < 15 ; $i++) { 
+        $rand = rand(0,100);
+        if (!in_array ($rand,$numbers)) {
+            $numbers[]=$rand;
+            echo $rand .' ';
+        }
+    }
+?>
+</p>
