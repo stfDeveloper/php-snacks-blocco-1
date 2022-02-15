e5d4c367431f3b25f4e8a9201d057382cc55c618
stfDeveloper <sstefancosmin@icloud.com>
2022-02-15 15:01:20 +0100

    snack 1

--- intro.php
+++ intro.php
@@ -1,12 +1,18 @@
+<!-- Creiamo un array contenente le partite di basket di un’ipotetica tappa del calendario. Ogni array avrà una squadra di casa e una squadra ospite, punti fatti dalla squadra di casa e punti fatti dalla squadra ospite. Stampiamo a schermo tutte le partite con questo schema.
+Olimpia Milano - Cantù | 55-60 -->
 <?php
-
 $squadre = [
-    ["name" =>"Olimpia","points"=> 53],
-    ["name" =>"Virtus" ,"points"=> 34],
-    ["name" =>"Lakers" ,"points"=> 56],
-    ["name" =>"Brindisi","points"=> 23],
-    ["name" =>"Allianz","points"=> 43],
-    ["name" =>"Fortitudo","points"=> 65],
-]
+    ['nameHome' =>"Olimpia",'pointsHome'=> 53],
+    ['nameGuest' =>"Virtus" ,'pointsGuest'=> 34],
+    ['nameHome' =>"Lakers" ,'pointsHome'=> 56],
+    ['nameGuest' =>"Brindisi",'pointsGuest'=> 23],
+    ['nameHome' =>"Allianz",'pointsHome'=> 43],
+    ['nameGuest' =>"Fortitudo",'pointsGuest'=> 65],
+] 
 ?>
-<h1> <?php echo $squadre ?></h1>
+<h1> <?php
+for ($i=0; $i <count($squadre); $i++) { 
+    echo $squadre[$i]['nameHome']." "." - "." ".$squadre[$i]['nameGuest']." | " .$squadre[$i]['pointsHome']."-".$squadre[$i]['pointsGuest'];
+}
+
+?></h1>
